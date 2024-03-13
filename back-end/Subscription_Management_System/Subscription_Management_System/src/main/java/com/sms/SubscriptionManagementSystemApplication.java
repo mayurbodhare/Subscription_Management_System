32e14a0732cbd353200c9d8231dd883942f1de2e
Mayur Bodhare <mrbodhre7@gmail.com>
2024-03-13 14:20:58 +0530

    ModelMapper added

--- back-end/Subscription_Management_System/Subscription_Management_System/src/main/java/com/sms/SubscriptionManagementSystemApplication.java
+++ back-end/Subscription_Management_System/Subscription_Management_System/src/main/java/com/sms/SubscriptionManagementSystemApplication.java
@@ -1,13 +1,24 @@
 package com.sms;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class SubscriptionManagementSystemApplication {
 
 	public static void main(String[] args) {
+		log.info("Inside Main App!!!");
 		SpringApplication.run(SubscriptionManagementSystemApplication.class, args);
+	}
+	
+	@Bean
+	private static ModelMapper getMapper() {
+		return new ModelMapper();
 	}
 
 }
