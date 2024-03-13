ab92e402727b5c8c08ffd46874e83d0eb5aefc25
Harish <itsharishk1@gmail.com>
2024-03-13 14:57:09 +0530

    UserRepository and RelationRepository added

---
+++ back-end/Subscription_Management_System/Subscription_Management_System/src/main/java/com/sms/repository/RelationRepository.java
@@ -0,0 +1,9 @@
+package com.sms.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sms.entity.RelationEntity;
+
+public interface RelationRepository extends JpaRepository<RelationEntity, Integer> {
+ 
+}
