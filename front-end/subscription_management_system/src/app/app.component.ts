a0b548e2d1309e364481741c62308936053305a5
Mayur Bodhare <mrbodhre7@gmail.com>
2024-03-19 10:30:44 +0530

    conflicts resolved in ui

--- front-end/subscription_management_system/src/app/app.component.ts
+++ front-end/subscription_management_system/src/app/app.component.ts
@@ -2,22 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { LoginComponent } from './login/login.component';
-<<<<<<< HEAD
 import { SignUpComponent } from "./sign-up/sign-up.component";
-=======
 import { CardComponent } from "./card/card.component";
->>>>>>> 4c99817432cb270d9f4bc707f9bb0f69d400a673
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-<<<<<<< HEAD
-    imports: [RouterOutlet, MatSlideToggleModule, LoginComponent, SignUpComponent]
-=======
-    imports: [RouterOutlet, MatSlideToggleModule, LoginComponent, CardComponent]
->>>>>>> 4c99817432cb270d9f4bc707f9bb0f69d400a673
+    imports: [RouterOutlet, MatSlideToggleModule, LoginComponent, SignUpComponent, CardComponent]
 })
 export class AppComponent {
   title = 'subscription_management_system';
--- front-end/subscription_management_system/src/app/login/login.component.ts
+++ front-end/subscription_management_system/src/app/login/login.component.ts
@@ -8,12 +8,6 @@ import {
   ReactiveFormsModule,
 } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
-<<<<<<< HEAD
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-=======
-// import { MatIconModule } from '@angular/material/icon';
->>>>>>> 4c99817432cb270d9f4bc707f9bb0f69d400a673
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { UserService } from '../../services/user.service';
@@ -45,30 +39,10 @@ export class LoginComponent {
   password = '';
   userDTO!: UserDTO;
 
-<<<<<<< HEAD
   loginUser() {
     this.email =
       this.emailFormControl.value !== null ? this.emailFormControl.value : '';
 
-=======
-  // async loginUser(){
-
-  //   this.email = this.emailFormControl.value !== null ? this.emailFormControl.value: "";
-
-  //   console.log(this.email);
-  //   console.log(this.password);
-
-  //   const something = await this.userService.loginUser(this.email, this.password);
-  //   this.userDTO = this.userService.loggedInUser;
-  //   console.log(this.userDTO);
-
-  // }
-
-  loginUser() {
-    this.email =
-      this.emailFormControl.value !== null ? this.emailFormControl.value : '';
-
->>>>>>> 4c99817432cb270d9f4bc707f9bb0f69d400a673
     this.userService
       .loginUser(this.email, this.password)
       .subscribe((response) => {
