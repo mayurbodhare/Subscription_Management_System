4c99817432cb270d9f4bc707f9bb0f69d400a673
Harish <itsharishk1@gmail.com>
2024-03-19 10:16:45 +0530

    plan card created

--- front-end/subscription_management_system/src/app/app.component.ts
+++ front-end/subscription_management_system/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { LoginComponent } from './login/login.component';
+import { CardComponent } from "./card/card.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, MatSlideToggleModule, LoginComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, MatSlideToggleModule, LoginComponent, CardComponent]
 })
 export class AppComponent {
   title = 'subscription_management_system';
