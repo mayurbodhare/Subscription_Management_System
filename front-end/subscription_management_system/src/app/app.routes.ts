ab2fd19e07104c7874f1912e91a10bf0a7f87bd5
Harish <itsharishk1@gmail.com>
2024-03-19 15:20:32 +0530

    admin changes

--- front-end/subscription_management_system/src/app/app.routes.ts
+++ front-end/subscription_management_system/src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
+import { PlanFormComponent } from './admin/plan-form/plan-form.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignUpComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'admindashboard', component: AdminDashboardComponent },
+  { path: 'planform', component: PlanFormComponent },
 ];
